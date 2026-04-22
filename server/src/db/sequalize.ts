755815516321acdd85d512a8db39ea02c3ee431d
Stas Borychevskyi <sborichevskiy74@gmail.com>
2026-04-22 18:59:05 +0300

    Added db models

--- server/src/db/sequalize.ts
+++ server/src/db/sequalize.ts
@@ -1,29 +1,29 @@
-
-import { Sequelize } from 'sequelize-typescript';
-import { User } from './models/User';
-import { UserProfile } from './models/UserProfile';
-import { Job } from './models/Job';
-import { Application } from './models/Application';
-import { RefreshToken } from './models/RefreshToken';
-import { Comment } from './models/Comment';
+import { Sequelize } from "sequelize-typescript";
+import { User } from "./models/User";
+import { UserProfile } from "./models/UserProfile";
+import { Job } from "./models/Job";
+import { Application } from "./models/Application";
+import { RefreshToken } from "./models/RefreshToken";
+import { Comment } from "./models/Comment";
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-  dialect: 'postgres',
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
+    models: [User, UserProfile, Job, Application, RefreshToken, Comment],
   },
 });
 
 export const initDb = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Database connected');
+    console.log("✅ Database connected");
     await sequelize.sync();
   } catch (err) {
-    console.error('❌ Database connection failed:', err);
+    console.error("❌ Database connection failed:", err);
   }
 };
 
