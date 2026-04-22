1f8d40589d4df5d273365ea4ff9940f71ad127ca
Stas Borychevskyi <sborichevskiy74@gmail.com>
2026-04-22 19:15:25 +0300

    Edited SMTP

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -16,7 +16,7 @@ export interface SendActivationEmailParams {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT) || 587,
-  secure: process.env.SMTP_PORT === '465',
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
