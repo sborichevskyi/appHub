d8f3c4c161855b08c65f7f0b35c675bf00271ff4
Stas Borychevskyi <sborichevskiy74@gmail.com>
2026-04-27 15:20:14 +0300

    Added jobWorker into package.json

--- server/src/queue/jobQueue.ts
+++ server/src/queue/jobQueue.ts
@@ -1,19 +1,13 @@
-import dotenv from 'dotenv';
-import Queue from 'bull';
+import dotenv from "dotenv";
+import Queue from "bull";
 dotenv.config();
 
-export const jobQueue = new Queue('fetch-jobs', {
-  redis: { 
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    password: process.env.REDIS_PASSWORD,
-   },
+export const jobQueue = new Queue("fetch-jobs", process.env.REDIS_URL as string);
+
+jobQueue.on("error", (err) => {
+  console.error("Redis connection error:", err);
 });
 
-jobQueue.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
-
-jobQueue.on('ready', () => {
-  console.log('✅ Connected to Redis at', jobQueue.client.options);
+jobQueue.on("ready", () => {
+  console.log("✅ Connected to Redis at", jobQueue.client.options);
 });
